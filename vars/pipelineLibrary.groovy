bd30e83913bd606c47cc584e0c4ac4d398dcc9f4
Nils Haug <mail@n-haug.de>
2022-11-15 11:58:51 +0100

    Update pipelineLibrary.groovy

--- vars/pipelineLibrary.groovy
+++ vars/pipelineLibrary.groovy
@@ -1,35 +1,44 @@
-pipeline {
-    agent { label "nhaug" }
-    stages {
-        stage("cleanup") {
-            steps {
-                echo "Vor clean"
-                sh "ls -al"
-                cleanWs()
-                echo "Nach Clean"
-                sh "pwd"
-                sh "ls -al"
+import com.jenkins.*
+    
+def call (body) {
+    LinkedHashMap config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    pipeline {
+        agent { label "nhaug" }
+        stages {
+            stage("cleanup") {
+                steps {
+                    echo "Vor clean"
+                    sh "ls -al"
+                    cleanWs()
+                    echo "Nach Clean"
+                    sh "pwd"
+                    sh "ls -al"
+                }
+            }
+            stage("checkout") {
+                steps {
+                    echo "jetzt muss checkout erfolgen"
+                }
+            }
+            stage("start") {
+                steps {
+                    echo "jetzt wirklich"
+                }
+            }
+            stage("build") {
+                steps {
+                    echo "build"
+                }
             }
         }
-        stage("checkout") {
-            steps {
-                echo "jetzt muss checkout erfolgen"
+        post {
+            always {
+                sh 'printenv'
             }
-        }
-        stage("start") {
-            steps {
-                echo "jetzt wirklich"
-            }
-        }
-        stage("build") {
-            steps {
-                echo "build"
-            }
-        }
-    }
-    post {
-        always {
-            sh 'printenv'
         }
     }
 }
